646db947d1f592caf328936bab6d85bbdbc0e9d0
YZ-Bruh <131263805+YZ-Bruh@users.noreply.github.com>
2023-09-01 16:28:22 +0300

    Update and rename twrp_oakland_64.mk to omni_oakland_64.mk

--- omni_oakland_64.mk
+++ omni_oakland_64.mk
@@ -10,7 +10,7 @@ $(call inherit-product, $(SRC_TARGET_DIR)/product/core_64_bit.mk)
 $(call inherit-product, $(SRC_TARGET_DIR)/product/full_base_telephony.mk)
 
 # Inherit some common Omni stuff.
-$(call inherit-product, vendor/twrp/config/common.mk)
+$(call inherit-product, vendor/omni/config/common.mk)
 
 # Inherit from oakland_64 device
 $(call inherit-product, device/tcl/oakland_64/device.mk)
@@ -20,10 +20,3 @@ PRODUCT_NAME := twrp_oakland_64
 PRODUCT_BRAND := alps
 PRODUCT_MODEL := oakland_64
 PRODUCT_MANUFACTURER := tcl
-
-PRODUCT_GMS_CLIENTID_BASE := android-tcl
-
-PRODUCT_BUILD_PROP_OVERRIDES += \
-    PRIVATE_BUILD_DESC="vnd_oakland_64-user 11 RP1A.200720.011 7K2Q release-keys"
-
-BUILD_FINGERPRINT := TCL/T766H_RU/Oakland:11/RP1A.200.720.011/7K2Q:user/release-keys
